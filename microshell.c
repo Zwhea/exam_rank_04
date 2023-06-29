97903f6a5360dde409159585bbb0913f5d5fb3d2
Thea Wang <theawang@yahoo.com>
2023-06-29 11:17:51 +0200

    erase comments

--- microshell.c
+++ microshell.c
@@ -17,12 +17,10 @@ void	ft_putstr_fd(int fd, char *s)
 
 void	cd_builtin(char **av)
 {
-	////verif si le nb arg est bon
 	if ((!av[1] || av[2]))
 		ft_putstr_fd(2, "error: cd: bad arguments\n");
 	else
 	{
-		////changer de dir et proteger fonction
 		if (chdir(av[1]) != 0)
 		{
 			ft_putstr_fd(2, "error: cd: cannot change directory to ");
@@ -30,27 +28,20 @@ void	cd_builtin(char **av)
 			ft_putstr_fd(2, "\n");
 		}
 	}
-	return ;
 }
 
 void	execute(char **av, char **env, bool is_last_cmd)
 {
-	int	pid;
-	int	fd[2];
+	pid_t	pid;
+	int		fd[2];
 
-	////si c'est la buitlin
 	if (strcmp(av[0], "cd") == 0)
-	{
-		cd_builtin(av);
-		return ;
-	}
-	////sinon pipe
+		return (cd_builtin(av));
 	if (pipe(fd) == -1)
 	{
-		ft_putstr_fd(2, "error fatal\n");
+		ft_putstr_fd(2, "error: fatal\n");
 		exit(0);
 	}
-	////fork et protection fork
 	pid = fork();
 	if (pid < 0)
 	{
@@ -59,21 +50,17 @@ void	execute(char **av, char **env, bool is_last_cmd)
 		close(fd[1]);
 		exit(0);
 	}
-	////process parent : close 1, dup, waitpid
 	if (pid > 0)
 	{
 		close(fd[1]);
-		////dup si on n'est pas sur la derniere commande - in
 		if (is_last_cmd == false)
 			dup2(fd[0], STDIN_FILENO);
 		close(fd[0]);
 		waitpid(-1, 0, 0);
 	}
-	////process child : close 0, dup, execve
 	else
 	{
 		close(fd[0]);
-		////dup si on n'est pas sur la derniere commande - out
 		if (is_last_cmd == false)
 			dup2(fd[1], STDOUT_FILENO);
 		close(fd[1]);
@@ -94,30 +81,23 @@ int	main(int ac, char **av, char **env)
 
 	while (i < ac)
 	{
-		////cas: on a un ";"
 		if ((strcmp(av[i], ";")) == 0)
 		{
-			////met a NULL, que l'on envoie a execute jusqu'a NULL
 			av[i] = NULL;
-			////on verifie qu'on ne soit pas sur un autre ";"
 			if (av[j] && strcmp(av[j], ";") != 0)
 				execute(&av[j], env, true);
-				////il s'agit bien de la derniere commande de la ligne
 			j = i + 1;
 		}
-		////cas: on a un "|"
 		else if ((strcmp(av[i], "|") == 0))
 		{
 			av[i] = NULL;
-			execute(&av[j], env, false);
-			////il y a encore des commandes apres
+			if (av[j] && strcmp(av[j], "|") != 0)
+				execute(&av[j], env, false);
 			j = i + 1;
 		}
 		i++;
 	}
-	////cas: une seule commande sans ";" ou "|"
 	if (j < ac && av[j])
 		execute(&av[j], env, true);
-		////il s'agit bien de la derniere commande de la ligne, il n'y en a qu 'une
 	return (0);
 }
